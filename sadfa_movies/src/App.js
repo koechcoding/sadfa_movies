42f0a2cc57d0f3b0fe25f115a5ac43c6e689800c
koechcoding <koechkelvin2001@gmail.com>
2022-12-04 01:58:54 +0300

    adding routes

--- sadfa_movies/src/App.js
+++ sadfa_movies/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Link } from "react-router-dom";
+import DisplayMovies  from './DisplayMovies';
+import Home  from './Home';
 
-function App() {
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/movies">Movies</Link></li>
+        </ul>
+      </nav>
+   
+
+      <Routes>
+        <Route path="/movies" element={<DisplayMovies />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  )  
 }
 
 export default App;
